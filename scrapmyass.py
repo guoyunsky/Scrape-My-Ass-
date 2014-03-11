c2f648d7bbc5be310baf0bee9b67005e9991a849
guoyun <guoyun@guoyun.(none)>
2014-03-11 11:00:39 +0800

    support use http proxy to request hidemyass.com and use arguments to run

--- scrapmyass.py
+++ scrapmyass.py
@@ -2,6 +2,7 @@ import urllib2
 import lxml.html as html
 import re
 import socket
+import argparse
 
 
 def replaceme(source):
@@ -19,8 +20,18 @@ def replaceme(source):
 
     return source
 
-def hidemyass(url):
-    source = urllib2.urlopen(url).read()
+def hidemyass(url, proxy = None):
+    if proxy:
+        proxies = {"http":"http://%s" % proxy}
+        headers={'User-agent' : 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; en) Opera 9.50'}
+        proxy_support = urllib2.ProxyHandler(proxies)
+        opener = urllib2.build_opener(proxy_support, urllib2.HTTPHandler(debuglevel=1))
+        urllib2.install_opener(opener)
+        req = urllib2.Request(url, None, headers)
+        source = urllib2.urlopen(req).read()
+    else:
+        source = urllib2.urlopen(url).read()
+        
     source = html.fromstring(replaceme(source))
     list = []
     for tr in source.xpath('//tr'):
@@ -36,22 +47,64 @@ def hidemyass(url):
 		list.append([ip, anonimity])
     return list
 
-def proxy(url,proxy):
+def check_proxy(url, proxy):
+    check_result = False
     try:
-        proxy_handler = urllib2.ProxyHandler({'http': proxy})
+        proxies = {"http":"http://%s" % proxy}
+        proxy_handler = urllib2.ProxyHandler(proxies)
         proxy_auth_handler = urllib2.ProxyBasicAuthHandler()
         request = urllib2.build_opener(proxy_handler, proxy_auth_handler)
         request.addheaders= [('User-agent', "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; en) Opera 9.50")]
-        return request.open(url).read()
+        
+        response = request.open(url).read()
+        if response and proxy[0][:8] in response:
+            check_result = True
     except (urllib2.URLError, urllib2.HTTPError), e:
-        return
+        return check_result
     except Exception, detail:
-        return
+        return check_result
+        
+    return check_result
+    
+def format_proxy(source_proxy):
+    return "http://%s\n" % source_proxy
 
-socket.setdefaulttimeout(3)
-proxylist = hidemyass("http://hidemyass.com/proxy-list")
-
-for i in xrange(len(proxylist)):
-    website = proxy("http://www.whatismyip.com/", proxylist[i][0])
-    if website is not None and proxylist[i][0][:8] in website:
-        print [i],proxylist[i][0], proxylist[i][1]
+def main(proxy=None, output_file_path=None, check=False):
+    socket.setdefaulttimeout(10)
+    proxylist = hidemyass("http://hidemyass.com/proxy-list", proxy)
+    
+    file = None
+    if output_file_path:
+        file = open(output_file_path, 'w+')
+    
+    output_proxylist = []
+    for i in xrange(len(proxylist)):
+        append = False
+        if check:
+            check_result = check_proxy("http://www.whatismyip.com/", proxylist[i][0])
+            
+        if not check or check_result:
+            output_proxylist.append(proxylist[i])
+        
+    
+    if len(output_proxylist) > 0:
+        if output_file_path:
+             file = open(output_file_path, 'w')
+             
+        for proxy in output_proxylist:
+            if file:
+                file.write(format_proxy(proxy[0]))
+            else:
+               print proxy[0], proxy[1][0], proxy[1][1]
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Scrape proxy list of hidemyass.com and check if proxy is " 
+        " working or not.for example:\n./scrapmyass -p 192.168.1.3:8080 -o /tmp/proxy.txt -c True")
+    parser.add_argument('-p','--proxy', help='http proxy to request hidemyass.com', required=False)
+    parser.add_argument('-o','--output_file', help='the file path which will writed http proxy', required=False)
+    parser.add_argument('-c','--check', help='whether check the http proxy', required=False, action='store_true')
+    args = parser.parse_args()
+    
+    main(args.proxy, args.output_file, args.check)
+    
+   
